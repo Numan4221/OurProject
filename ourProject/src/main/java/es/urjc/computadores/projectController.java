6c33afad473b1afc5c30782c9c32d29d165da439
Axel <axelsax1998@gmail.com>
2020-02-13 19:04:07 +0100

    Controller para la página de proyecto iniciado
    Recoge información de la base de datos y la muestra en la página, faltan comentarios, metas, y botón de donación

--- ourProject/src/main/java/es/urjc/computadores/projectController.java
+++ ourProject/src/main/java/es/urjc/computadores/projectController.java
@@ -1,17 +1,32 @@
 package es.urjc.computadores;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class projectController {
 
+	@Autowired
+	private ProjectRepository projectRepo;
+	
 	@RequestMapping("/ourProject/project")
-	public String initSession(Model model) {
+	public String initSession(Model model, @RequestParam long id) {
 		
 		//model.addAttribute(attributeName, attributeValue);
+		Optional<Project> proyecto = projectRepo.findById(id);
+		Project proyectoReal = proyecto.get();
 		
-		return "project";
+		model.addAttribute("titulo", proyectoReal.projectName);
+		model.addAttribute("desc", proyectoReal.description);
+		model.addAttribute("goals", proyectoReal.goals);
+		model.addAttribute("moneyCollected", proyectoReal.moneyCollected);
+		model.addAttribute("developer", proyectoReal.developer.nickname);
+		
+		return "paginaProyecto";
 	}
 }
